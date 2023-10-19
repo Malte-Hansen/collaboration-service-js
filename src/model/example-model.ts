05420b36cb751910ef723e602a42071b489228da
Alexander Krause <akr@informatik.uni-kiel.de>
2023-10-19 14:04:10 +0200

    re-add example-model

---
+++ src/model/example-model.ts
@@ -0,0 +1,15 @@
+export class Example {
+  private value: number;
+
+  constructor() {
+    this.value = 0;
+  }
+
+  getValue(): number {
+    return this.value;
+  }
+
+  setValue(value: number): void {
+    this.value = value;
+  }
+}
