d8b256b137141c8d51bf21a70936ed10f384928c
Malte Hansen <malte.hanseat@gmail.com>
2024-06-07 13:27:59 +0200

    Remove duplicate and unused test configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,26 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  testEnvironment: 'node',
-  preset: 'ts-jest',
-  rootDir: './',
-  modulePaths: ['<rootDir>'],
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  moduleFileExtensions: ['js', 'json', 'ts'],
   moduleNameMapper: {
     '^src$': '<rootDir>/src',
     '^src/(.+)$': '<rootDir>/src/$1',
   },
   modulePathIgnorePatterns: ['src/typings'],
+  modulePaths: ['<rootDir>'],
+  preset: 'ts-jest',
+  rootDir: './',
+  testEnvironment: 'node',
   testPathIgnorePatterns: [
     '/node_modules./',
     '<rootDir>/(coverage|dist|lib|tmp)./',
   ],
+  testRegex: '.supertest-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
 };
 
 export default config;
