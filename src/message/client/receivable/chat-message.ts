c08e152e6a9bff8ad93ff0532a598b36b519b4e9
Luca <stu211622@mail.uni-kiel.de>
2024-07-09 20:38:13 +0200

    Receivable Chat Message / Sendable Chat Sync Response: Added userName

--- src/message/client/receivable/chat-message.ts
+++ src/message/client/receivable/chat-message.ts
@@ -3,5 +3,6 @@ export const CHAT_MESSAGE_EVENT = 'chat_message';
 export type ChatMessage = {
   userId: string;
   msg: string;
+  userName: string;
   timestamp: string;
 };
--- src/message/client/sendable/chat-sync-response.ts
+++ src/message/client/sendable/chat-sync-response.ts
@@ -3,5 +3,6 @@ export const CHAT_SYNC_EVENT = 'chat_synchronization';
 export type ChatSynchronizeResponse = {
     userId: string;
     msg: string;
+    userName: string;
     timestamp: string;
 };
